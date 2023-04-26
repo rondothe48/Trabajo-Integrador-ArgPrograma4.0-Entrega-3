5888f9e29a4e409117e5a44b7d8ef2b57f86be3a
Ignacio Scheffer <ignacioscheffer2004@gmail.com>
2023-04-26 20:32:02 -0300

    Update Dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,23 @@ package org.example;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String pronosticosFile = "Proyecto\\src\\pronostico.csv"; // Ruta del archivo CSV de pronósticos
-        String resultadosFile = "Proyecto\\src\\resultados.csv"; // Ruta del archivo CSV de resultados
+
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/argpro", "root", "332211");
+            Statement statement = con.createStatement();
+            statement.execute("SELECT *");
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        String pronosticosFile = "src/main/java/org/example/pronostico.csv"; // Ruta del archivo CSV de pronósticos
+        String resultadosFile = "src/main/java/org/example/resultados.csv"; // Ruta del archivo CSV de resultados
 
         // Crear una instancia de la clase LectorCSV
         LectorCSV lectorCSV = new LectorCSV(pronosticosFile, resultadosFile);
